735152251af3088a40ae01b12fb75185fff97b1f
Vlad <vladislav.zhyla@gmail.com>
2023-09-29 16:08:38 +0300

    implemented views and serializers for apps

---
+++ revenue/serializers.py
@@ -0,0 +1,11 @@
+from rest_framework import serializers
+
+
+class RevenueSerializer(serializers.Serializer):
+    name = serializers.CharField(max_length=255)
+    date = serializers.DateField()
+    revenue_sum = serializers.DecimalField(max_digits=9, decimal_places=2)
+    spend_sum = serializers.DecimalField(max_digits=10, decimal_places=2)
+    impressions_sum = serializers.IntegerField()
+    clicks_sum = serializers.IntegerField()
+    conversion_sum = serializers.IntegerField()
--- spend/views.py
+++ spend/views.py
@@ -1,3 +1,21 @@
-from django.shortcuts import render
+from django.db.models import Sum
+from rest_framework import status
+from rest_framework.response import Response
+from rest_framework.views import APIView
 
-# Create your views here.
+from spend.models import SpendStatistic
+from spend.serializers import SpendSerializer
+
+
+class SpendView(APIView):
+    def get(self, request):
+        queryset = SpendStatistic.objects.values("date", "name").annotate(
+            spend_sum=Sum("spend"),
+            impressions_sum=Sum("impressions"),
+            clicks_sum=Sum("clicks"),
+            conversion_sum=Sum("conversion"),
+            revenue_sum=Sum("revenuestatistic__revenue", default=0),
+        )
+        serializer = SpendSerializer(queryset, many=True)
+
+        return Response(serializer.data, status=status.HTTP_200_OK)
